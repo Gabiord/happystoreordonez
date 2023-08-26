f41340f73f817cf4675250fdbc49ee12d6ad53a6
gabiord <gabiord9@gmail.com>
2023-08-26 15:32:29 -0300

    pruebas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -237,12 +237,11 @@ function NavBar(children) {
               <div className="ml-auto flex items-center">
                 <div class="space-y-6 border-t border-gray-200 px-4 py-6">
                   <div class="flow-root">
-                    <a
-                      href="/session"
-                      class="-m-2 block p-2 font-medium text-gray-900"
+                  <Link to="/session"
+                      className="-m-2 block p-2 font-medium text-gray-900"
                     >
                       Iniciar Sesion
-                    </a>
+                  </Link>
                   </div>
                 </div>
 
