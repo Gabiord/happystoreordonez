0591e2de107f0e0837590da7fc085a62750e038d
gabiord <gabiord9@gmail.com>
2023-08-26 01:12:46 -0300

    actualizacion de links

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ function Main(children) {
     return (
         <main>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/happystoreordonez" element={<LandingPage />} />
                 <Route path="/products" element={<ItemListContainer />} />
                 <Route path="products/category/:id" element={<ItemListContainer />} />
                 <Route path="/products/:id" element={<ItemDetailContainer />} />
